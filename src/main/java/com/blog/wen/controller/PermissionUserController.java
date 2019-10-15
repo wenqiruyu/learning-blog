fd08d4cfbd86240963e2dbaf22197506c17e3823
Allen <yingxing.liu1@cignacmb.com>
2019-10-15 18:35:29 +0800

    完成用户注册功能

--- src/main/java/com/blog/wen/controller/PermissionUserController.java
+++ src/main/java/com/blog/wen/controller/PermissionUserController.java
@@ -16,28 +16,18 @@ import java.util.Map;
  * 修改备注：
  */
 @RestController
-//@RequestMapping("/user")
+@RequestMapping("/user")
 public class UserController {
 
     @PostMapping("/register")
     public ResultVO registerUser(@RequestBody Map<String,String> registerUser){
-        /*User user = new User();
-        user.setUsername(registerUser.get("username"));
-        // 记得注册的时候把密码加密一下
-        user.setPassword(new BCryptPasswordEncoder().encode(registerUser.get("password")));
-        user.setRole("ROLE_USER");
-        userService.addUser(user);*/
-        return ResultVO.createBySuccess();
-    }
 
-    @PostMapping("/emp/adv")
-    public ResultVO empAdv(){
-        /*User user = new User();
+        User user = new User();
         user.setUsername(registerUser.get("username"));
         // 记得注册的时候把密码加密一下
         user.setPassword(new BCryptPasswordEncoder().encode(registerUser.get("password")));
         user.setRole("ROLE_USER");
-        userService.addUser(user);*/
+        userService.addUser(user);
         return ResultVO.createBySuccess();
     }
 }
